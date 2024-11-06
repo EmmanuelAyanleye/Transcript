cf0a44e1d2e33062e243dcec8d385cef517b4e3d
Emmanuel Ayanleye <ayanleyeemmanuel5@gmail.com>
2024-11-06 12:20:17 +0100

    Python Class

---
+++ main.py
@@ -0,0 +1,71 @@
+# # Age = int(input("Enter the Age"))
+
+# # if Age>18:
+# #     print ("Valid")
+# # elif Age==18:
+# #     print ("Come next Year")
+# # else:
+# #     print ("in-Valid")
+
+# # # IF statement
+# # # If else Statement (elif)
+# # resques username and password from 
+
+# username = input("Enter Username:")
+# password = input("Enter password:")
+
+# if username == ("User123"):
+#     print("You are welcome")
+
+# elif username != ("Pass123"):
+#     print("Try your username again")
+
+
+# elif password == ("Pass123"):
+#     print("You are Welcome")
+
+# elif password != ("User123"):
+#     print("Try Your Password Again")
+
+# else:
+#     print("The username and Pasword is not valid")
+
+
+#FOR LOOPING IN PYTHON
+# mylist = ["Emman", "Ayaneye", "Iyanu"]
+# word = "Hello"
+# for letter in word:
+#     print(letter)
+
+# for name in mylist:
+#     print(name)
+
+# for i in range (20):
+#     if i%2 = 0:
+#         print(i)
+
+# WHILE LOOP: It's used to repeat a block of code until a certain condition is met
+# Variable Intialization
+# i = 0
+# while i<5:
+#     print(i)
+#     i+=1
+
+# i = 2
+# while i<20:
+#     print(i)
+#     i+=2
+
+
+# BREAK: It's used to exit a loop
+for i in range (5):
+    if i == 3:
+        print(i)
+        break
+
+for i in range (5):
+    print(i)
+    if i == 3:
+        break
+
+# CONTINUE: It's used to skip the rest of the code in a loop and start the next iteration
